362118892922823b8b25b5abdeb9f1ce48d7cd53
setoffonom <setoffonom@gmail.com>
2014-04-06 17:15:29 +0400

    First OM

--- Divya Loka/DLViewController.h
+++ Divya Loka/DLViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface DLViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *symbolWebView;
 
 @end
--- Divya Loka/DLViewController.m
+++ Divya Loka/DLViewController.m
@@ -17,13 +17,40 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    NSString* filePath = [[NSBundle mainBundle] pathForResource:@"credo" ofType:@"rtf"];
+    NSURL *url = [NSURL fileURLWithPath:filePath];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [_symbolWebView loadRequest:request];
+    [_symbolWebView setScalesPageToFit:YES];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    UIBarButtonItem* rightButton = [[UIBarButtonItem alloc]
+                                    initWithTitle:@"Принять"
+                                    style:UIBarButtonItemStylePlain
+                                    target:self action:@selector(doneButton)];
+    
+    [rightButton setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+                                         [UIFont fontWithName:@"Trebuchet MS" size:18], NSFontAttributeName,
+                                         [UIColor colorWithRed:56.0/255.0 green:56.0/255.0 blue:255.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
+                                         nil]
+                               forState:UIControlStateNormal];
+    self.navigationItem.rightBarButtonItem = rightButton;
+}
+
+
+- (void) doneButton
+{
+    [self performSegueWithIdentifier:@"DLDirectoryTableViewController" sender:nil];
 }
 
 @end
